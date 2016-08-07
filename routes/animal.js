ea0144de4027be339b8eb561ff398620e81eab59
Ngoc Don <kaitoukid2701@gmail.com>
2016-08-08 01:19:27 +0700

    Done inserting new Animal

--- routes/animal.js
+++ routes/animal.js
@@ -9,14 +9,13 @@ var Animal = mongoose.model('Animal');
 var IMG_FIELDS = [
 	{name: 'hinhVe', animalSchemaProp: 'duLieuPhanTichMau.hinhVe'},
 	{name: 'dinhKemXuLy', animalSchemaProp: 'xuLyCheTac.dinhKemXuLy'},
-	{name: 'hinhAnhDinhKem', animalSchemaProp: ''},
-	{name: 'dinhKemChayTrinhTuDNA', animalSchemaProp: ''},
-	{name: 'dinhKemTrinhTuDNA', animalSchemaProp: ''}
+	{name: 'hinhAnhDinhKem', animalSchemaProp: 'media.xuLyCheTac.hinhAnhDinhKem'},
+	{name: 'dinhKemChayTrinhTuDNA', animalSchemaProp: 'media.thongTinDNA.dinhKemChayTrinhTuDNA'},
+	{name: 'dinhKemTrinhTuDNA', animalSchemaProp: 'media.thongTinDNA.dinhKemTrinhTuDNA'},
+	{name: 'taiLieuPhanTich', animalSchemaProp: 'duLieuPhanTichMau.taiLieuPhanTich'}
 ];
 
-var PROP_FIELDS = [
-	{name: , animalSchemaProp: ''}
-]
+var PROP_FIELDS = JSON.parse(fs.readFileSync(path.join(__dirname, '../models/AnimalSchemaProps.json')).toString());
 
 module.exports = function (router) {
 
@@ -26,7 +25,20 @@ router.post('/dong-vat',
 		return preArray;
 	}, [])), 
 	function (req, res, next) {
+		console.log('start');
+		console.log(req.body);
 		var newAnimal = new Animal();
+
+		// save props
+		PROP_FIELDS.map(function (element) {
+			var nodes = element.animalSchemaProp.split('.');
+			var lastProp = nodes.splice(nodes.length - 1, 1)[0];
+			var tree = nodes.join('.');
+			objectChild(newAnimal, tree)[lastProp] = req.body[element.name];
+		})
+
+		console.log(newAnimal);
+
 		newAnimal.save(function (err, result) {
 			if (err){
 				console.log(err);
@@ -35,9 +47,16 @@ router.post('/dong-vat',
 			// rename images
 			IMG_FIELDS.map(function (element) {
 				if (element.animalSchemaProp && req.files[element.name]){
+					console.log('changing name of: ', element.animalSchemaProp);
+					var nodes = element.animalSchemaProp.split('.');
+					var lastProp = nodes.splice(nodes.length - 1, 1)[0];
+					var tree = nodes.join('.');
+					objectChild(newAnimal, tree)[lastProp] = [];
 					rename(req.files[element.name], objectChild(newAnimal, element.animalSchemaProp), UPLOAD_DEST_ANIMAL, result.id);
 				}
 			})
+
+			console.log(newAnimal);
 
 			newAnimal.save(function (err, r) {
 				if (err){
@@ -47,6 +66,9 @@ router.post('/dong-vat',
 						error: 'Error while saving to database'
 					})
 				}
+
+				console.log(newAnimal);
+
 				res.status(200).json({
 						status: 'success'
 					})
@@ -58,7 +80,13 @@ router.post('/dong-vat',
 
 function rename (curFiles, schemaField, position, mongoId) {
 	console.log(schemaField);
-	schemaField.splice(0, schemaField.length); // delete all old elements
+	try {
+		schemaField.splice(0, schemaField.length); // delete all old elements
+	}
+	catch (e){
+		console.log(e);
+		schemaField = [];
+	}
 	for (var i = 0; i < curFiles.length; i++) {
 		var file = curFiles[i];
 		try {
@@ -77,7 +105,29 @@ function rename (curFiles, schemaField, position, mongoId) {
 function objectChild (object, tree) {
 	var nodes = tree.trim().split('.');
 	for (var i = 0; i < nodes.length; i++) {
-		object = object[nodes[i]];
+		// object = object[nodes[i]];
+		if (nodes[i] in object){
+			object = object[nodes[i]];
+		}
+		else {
+			object[nodes[i]] = {}
+		}
 	}
 	return object;
+}
+
+function generate (schema) {
+	if (typeof(schema) != 'object'){
+		tree = nodes.join('.');
+		arr.push({name: nodes[nodes.length - 1], animalSchemaProp: tree});
+	}
+	else {
+		for (var i in schema){
+			console.log("push ", i);
+			nodes.push(i);
+			generate(schema[i]);
+			console.log("pop ", nodes[nodes.length - 1]);
+			nodes.splice(nodes.length - 1, 1);
+		}
+	}
 }

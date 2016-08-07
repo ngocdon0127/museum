aba2f1c5bd6a04bc084db39952e58d5a29d76145
Hola Nguyen <kevinhoa95@gmail.com>
2016-08-07 16:58:49 +0700

    add select box for cities, districts

--- app/asset/js/controllers/FormController.js
+++ app/asset/js/controllers/FormController.js
@@ -1,6 +1,21 @@
-app.controller('AnimalFormCtrl', ['$scope', function ($scope) {
+app.controller('AnimalFormCtrl', ['$scope','$http', function ($scope, $http) {
+	$http.get('/app/database/place.json').then(function(res){
+		$scope.places =res.data;
+		console.log($scope.places);
+	}, function(res){
+		console.log(res);
+	});
+	$scope.cityChange = function(){
+		// console.log("change");
+		angular.forEach($scope.places, function (value) {
+			console.log
+			if (value.name == $scope.animalForm.city) {
+				$scope.districts = value.districts;
+			};
+		})
+	}
 	$scope.addPost = function(){
-		var fd = new FormData(document.getElementById('form-animals'));
+		var fd = new FormData(document.getElementById('form-animal'));
 		console.log(fd);
 		// $.ajax({
 		// 	url: AuthService.hostName + '/api/house',

3ccefaadcc5a4cc8e155f9e0e022c31158e29233
Ngoc Don <kaitoukid2701@gmail.com>
2016-08-09 15:20:18 +0700

    Role from files

--- config/acl.js
+++ config/acl.js
@@ -1,6 +1,18 @@
+var fs = require('fs');
+var path = require('path');
+
 module.exports = function (acl) {
-	acl.allow('guest', '/test', ['view', 'edit']);
-	acl.allow('content', '/test', ['edit']);
+
+	// init roles
+	roles = JSON.parse(fs.readFileSync(path.join(__dirname, 'acl.json')).toString());
+	for (var i = 0; i < roles.length; i++) {
+		var role = roles[i];
+		for (var j = 0; j < role.allows.length; j++) {
+			acl.allow(role.role, role.allows[j].resource, role.allows[j].actions);
+		}
+	}
+
+	// asign users to roles
 	acl.addUserRoles('57a5af51b89dbe602613affc', 'guest')
 	acl.addUserRoles('57a6b435a8afadd42a8cabc5', 'guest')
 }

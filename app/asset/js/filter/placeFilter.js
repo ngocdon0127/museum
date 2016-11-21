915048834590c85ada24603758ef7e38fc7069e6
-g <-g>
2016-11-21 08:42:56 +0700

    Fix add and edit form

--- app/asset/js/filter/placeFilter.js
+++ app/asset/js/filter/placeFilter.js
@@ -4,9 +4,14 @@ app.filter('customDistrict', function() {
   	// console.dir(input);
     if (!input) return input;
     if (!search) return input;
+    // var _tmp = {}
     var result = {};
+    // var result = [];
     angular.forEach(input, function(value, key) {
-      if (input[key].districtId == search) {
+      if (input[key].districtId == search) {      
+        // _tmp = JSON.parse(JSON.stringify(input[key]));
+        // _tmp["id"] = key;
+        // result.push(_tmp);
         result[key] = input[key];
       }
     });
@@ -17,9 +22,15 @@ app.filter('customCity', function() {
   return function(input, search) {
     if (!input) return input;
     if (!search) return input;
+    // var _tmp = {};
     var result = {};
     angular.forEach(input, function(value, key) {
       if (input[key].cityId == search) {
+        // _tmp = JSON.parse(JSON.stringify(input[key]));
+        // _tmp["id"] = key;
+        // console.log(_tmp);
+        // console.log(input[key]);
+        // result.push(input[key]);
         result[key] = input[key];
       }
     });

08e48707e8cc5d782d9704d23e57a8c68171dd12
Ngoc Don <kaitoukid2701@gmail.com>
2017-02-22 00:00:19 +0700

    Fix models

--- models/Soil.js
+++ models/Soil.js
@@ -22,7 +22,7 @@ module.exports = function (mongoose) {
 		},
 		soLuongChatLuong: {
 			soLuongTieuBan: Number,
-			loaiMau: String,
+			loaiMauVat: String,
 			kichThuocMauMixed:{
 				chieuDai: Number,
 				donVi_chieuDai: String,

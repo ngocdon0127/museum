bc6eecef8082c0d3bc2c8733dae30a6eab80d599
-g <kevinhoa95@gmail.com>
2017-04-13 11:00:34 +0700

    add home page, limit access to route base on user permision

--- app/app.config.js
+++ app/app.config.js
@@ -3,57 +3,118 @@ app.config(function($locationProvider, $stateProvider, $urlRouterProvider) {
 	$locationProvider.hashPrefix('!');
 	$stateProvider
 	.state('home', {
-		url: '/'
+		url: '/',
+		templateUrl: 'views/users/home.template.html'
 	})
 	.state('them-dong-vat', {
 		url: '/dong-vat',
 		templateUrl: 'views/users/add-animal-form.template.html',
-		controller: 'AnimalFormCtrl'
+		controller: 'AnimalFormCtrl',
+		access : {
+			sample : "dongvat",
+			action : "create",
+			redirectTo: "home",
+			restrict: true
+		}
 	})
 	.state('them-thuc-vat', {
 		url: '/thuc-vat',
 		templateUrl: 'views/users/add-vegetable-form.template.html',
-		controller: 'VegetableFormCtrl'
+		controller: 'VegetableFormCtrl',
+		access : {
+			sample : "thucvat",
+			action : "create",
+			redirectTo: "home",
+			restrict: true
+		}
 	})
 	.state('them-dia-chat', {
 		url: '/dia-chat',
 		templateUrl: 'views/users/add-geological-form.template.html',
-		controller: 'GeologicalFormCtrl'
+		controller: 'GeologicalFormCtrl',
+		access : {
+			sample : "diachat",
+			action : "create",
+			redirectTo: "home",
+			restrict: true
+		}
 	})
 	.state('them-tho-nhuong', {
 		url: '/tho-nhuong',
 		templateUrl: 'views/users/add-land-form.template.html',
-		controller: 'LandFormCtrl'
+		controller: 'LandFormCtrl',
+		access : {
+			sample : "thonhuong",
+			action : "create",
+			redirectTo: "home",
+			restrict: true
+		}
 	})
 	.state('them-co-sinh', {
 		url: '/co-sinh',
 		templateUrl: 'views/users/add-paleontological-form.template.html',
-		controller: 'PaleontologicalFormCtrl'
+		controller: 'PaleontologicalFormCtrl',
+		access : {
+			sample : "cosinh",
+			action : "create",
+			redirectTo: "home",
+			restrict: true
+		}
 	})
 	.state('chinh-sua-dong-vat', {
 		url: '/dong-vat/chinh-sua/:id',
 		templateUrl: 'views/users/edit/edit-animal-form.template.html',
-		controller: 'EditAnimalFormCtrl'
+		controller: 'EditAnimalFormCtrl',
+		access : {
+			sample : "dongvat",
+			action : "edit",
+			redirectTo: "home",
+			restrict: true
+		}
 	})
 	.state('chinh-sua-thuc-vat', {
 		url: '/thuc-vat/chinh-sua/:id',
 		templateUrl: 'views/users/edit/edit-vegetable-form.template.html',
-		controller: 'EditVegetableFormCtrl'
+		controller: 'EditVegetableFormCtrl',
+		access : {
+			sample : "thucvat",
+			action : "edit",
+			redirectTo: "home",
+			restrict: true
+		}
 	})
 	.state('chinh-sua-co-sinh', {
 		url: '/co-sinh/chinh-sua/:id',
 		templateUrl: 'views/users/edit/edit-paleontological-form.template.html',
-		controller: 'EditPaleontologicalFormCtrl'
+		controller: 'EditPaleontologicalFormCtrl',
+		access : {
+			sample : "cosinh",
+			action : "edit",
+			redirectTo: "home",
+			restrict: true
+		}
 	})
 	.state('chinh-sua-tho-nhuong', {
 		url: '/tho-nhuong/chinh-sua/:id',
 		templateUrl: 'views/users/edit/edit-land-form.template.html',
-		controller: 'EditLandFormCtrl'
+		controller: 'EditLandFormCtrl',
+		access : {
+			sample : "thonhuong",
+			action : "edit",
+			redirectTo: "home",
+			restrict: true
+		}
 	})
 	.state('chinh-sua-dia-chat', {
 		url: '/dia-chat/chinh-sua/:id',
 		templateUrl: 'views/users/edit/edit-geological-form.template.html',
-		controller: 'EditGeologicalFormCtrl'
+		controller: 'EditGeologicalFormCtrl',
+		access : {
+			sample : "diachat",
+			action : "edit",
+			redirectTo: "home",
+			restrict: true
+		}
 	})
 	.state('quan-ly', {
 		url: '/quan-ly-mau',
@@ -62,27 +123,57 @@ app.config(function($locationProvider, $stateProvider, $urlRouterProvider) {
 	.state('quan-ly-dong-vat', {
 		url: '/quan-ly-mau/dong-vat',
 		templateUrl: 'views/users/manage-post/manage-post.template.html',
-		controller: 'AnimalManageController'
+		controller: 'AnimalManageController',
+		access : {
+			sample : "dongvat",
+			action : "create",
+			redirectTo: "home",
+			restrict: true
+		}
 	})
 	.state('quan-ly-thuc-vat', {
 		url: '/quan-ly-mau/thuc-vat',
 		templateUrl: 'views/users/manage-post/manage-post.template.html',
-		controller: 'VegetableManageController'
+		controller: 'VegetableManageController',
+		access : {
+			sample : "thucvat",
+			action : "create",
+			redirectTo: "home",
+			restrict: true
+		}
 	})
 	.state('quan-ly-tho-nhuong', {
 		url: '/quan-ly-mau/tho-nhuong',
 		templateUrl: 'views/users/manage-post/manage-post.template.html',
-		controller: 'LandManageController'
+		controller: 'LandManageController',
+		access : {
+			sample : "thonhuong",
+			action : "create",
+			redirectTo: "home",
+			restrict: true
+		}
 	})
 	.state('quan-ly-dia-chat', {
 		url: '/quan-ly-mau/dia-chat',
 		templateUrl: 'views/users/manage-post/manage-post.template.html',
-		controller: 'GeologicalManageController'
+		controller: 'GeologicalManageController',
+		access : {
+			sample : "diachat",
+			action : "create",
+			redirectTo: "home",
+			restrict: true
+		}
 	})
 	.state('quan-ly-co-sinh', {
 		url: '/quan-ly-mau/co-sinh',
 		templateUrl: 'views/users/manage-post/manage-post.template.html',
-		controller: 'PaleontologicalManageController'
+		controller: 'PaleontologicalManageController',
+		access : {
+			sample : "cosinh",
+			action : "create",
+			redirectTo: "home",
+			restrict: true
+		}
 	})
 	.state('testover', {
 		url: '/bai-dang/test-over',
@@ -94,14 +185,27 @@ app.config(function($locationProvider, $stateProvider, $urlRouterProvider) {
 app.run(function($rootScope, $location, $state, $stateParams, $http, AuthService){
 	$rootScope.$state = $state;
     $rootScope.$stateParams = $stateParams;
-	$rootScope.$on('$stateChangeStart', function(event, next, current){
+	$rootScope.$on('$stateChangeStart', function(event, toState, toStateParams){
 		var url = AuthService.hostName + "/users/me?datatype=json"
-		$http.get(url).then(function success(res) {
-			$rootScope.restricted = res.data;
-			$rootScope.username = res.data.user.fullname;
+		AuthService.getRestrict().then(function success(res) {
+			$rootScope.restricted = res;
+			$rootScope.username = res.user.fullname;
+			
+			if (toState.access) {
+				var sample = toState.access.sample;
+				var action = toState.access.action;
+				if (!res.restrict[sample][action]) {
+					$state.go('home')
+				}
+			}
 		}, function error(err) {
 			console.log(err)
 		});
-		// console.log(username);
+		// console.log(toStateParams);
+		// if (toState.access.restrict && AuthService.getRestrict(toState.access.sample)) {
+		// 	console.log(toStateParams);
+		// 	$state.go('home');
+			
+		// }
 	});
 });

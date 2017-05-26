10a827a49a0ba68e59cfea0ea79464e9ed79aea4
-g <kevinhoa95@gmail.com>
2017-05-27 03:05:03 +0700

    test download file

--- app/asset/js/controllers/OfflineController.js
+++ app/asset/js/controllers/OfflineController.js
@@ -11,4 +11,23 @@ app.controller('OfflineCtrl', function ($scope) {
       	};
       	reader.readAsText(input.files[0]);
     };
+
+    $scope.download = function () {
+      var link = "/content/dong-vat/59153670d01e41313c837dc9?display=docx";
+      var xhr = new XMLHttpRequest();
+          xhr.onreadystatechange = function () {
+            if ((xhr.readyState == 4) && (xhr.status == 200)){
+              var disposition = xhr.getResponseHeader('Content-Disposition');
+              var file_names = disposition.split([';'])
+              var file_name = decodeURIComponent(file_names[2]);
+              var type = xhr.getResponseHeader('Content-Type');
+              console.log(type);
+              var blob = new Blob([xhr.response], {type: type});
+              saveAs(blob, file_name);
+            }
+          }
+          xhr.responseType = 'arraybuffer';
+          xhr.open('GET',link, true);
+          xhr.send();
+    }
 });

b799ffb506a9948c4245a0d991062785644d0817
Hola Nguyen <kevinhoa95@mgail.com>
2017-06-01 16:23:28 +0700

    fix export fields in server

--- init.js
+++ init.js
@@ -782,7 +782,7 @@ var exportFilePromise = (objectInstance, options, extension) => {
 	let paragraph = options.paragraph;
 	let objectModelName = options.objectModelName;
 	let printedProperties = options.req.body;
-	let printAll = ('body' in options.req) && (options.req.body.custom == 1)
+	let printAll = !(('body' in options.req) && (options.req.body.custom == 1))
 	return new Promise((RESOLVE, REJECT) => {
 		async (function (){
 			console.log("calling docx");

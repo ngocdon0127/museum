3c3a690762bcf2678503ad4c9d62f981dd51dad6
Ngoc Don <kaitoukid2701@gmail.com>
2017-10-04 15:29:01 +0700

    add ico + check coordination

--- bootstrap/init-env.js
+++ bootstrap/init-env.js
@@ -355,7 +355,20 @@ function createSaveOrUpdateFunction (variablesBundle) {
 				}
 				else {
 					console.log('Tọa độ rời rạc')
-					req.body[field.fieldName] = req.body[field.fieldName].toLowerCase();
+					req.body[field.fieldName] = req.body[field.fieldName].toLowerCase()
+					let matches = req.body[field.fieldName].match(/^([0-9 \-]+)(°|độ)([0-9 \-]+)('|phút)([0-9 \-]+)("|giây)$/);
+					if (!matches || (matches.length < 7)) {
+						return responseError(req, _UPLOAD_DESTINATION, res, 400, ['error', 'field'], [_LABEL[field.fieldName], ' không đúng định dạng', field.fieldName]);
+					}
+					let partDo = parseInt(matches[1]);
+					let partPhut = parseInt(matches[2]);
+					let partGiay = parseInt(matches[3]);
+					if (!Number.isInteger(partDo) ||
+							!Number.isInteger(partPhut) ||
+							!Number.isInteger(partGiay)
+						){
+						return responseError(req, _UPLOAD_DESTINATION, res, 400, ['error', 'field'], [_LABEL[field.fieldName] + ': Tọa độ phải là các số nguyên', field.fieldName])
+					}
 				}
 			}
 		}

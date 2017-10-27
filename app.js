52a6fd37b737b236c139e57996072ddb9176a2a2
Ngoc Don <kaitoukid2701@gmail.com>
2017-10-27 11:25:42 +0700

    test quickthumb

--- app.js
+++ app.js
@@ -68,7 +68,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const qt = require('quickthumb');
+app.use(qt.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
 	name: require('./config/config').app.sessionCookieName,
 	secret: "SecretKeyMy",
